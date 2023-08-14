5fc12340c214232f8a510bd8d1184436725c9b85
akashtijare <akashtijare708374@gmail.com>
2023-08-14 14:08:39 +0530

    confirmation page

--- let-it-bee/src/Pages/address/Address.jsx
+++ let-it-bee/src/Pages/address/Address.jsx
@@ -4,9 +4,11 @@ import CheckOut from "./CheckOut";
 import Payment from "./Payment";
 import PersonalInfo from "./PersonalInfo";
 
-const { useState } = require("react");
+import { useState } from "react";
+import Confirmation from "./Confimation";
 
 const Address = () => {
+    const [formData, setFormData] = useState({});
 
     const [step, setStep] = useState(1);
 
@@ -18,26 +20,44 @@ const Address = () => {
         setStep(step - 1);
     };
 
+    const handleSubmit = (data) => {
+        setFormData(data);
+        
+      };
+    
+
     return (
-        <Box className="mainContainer">
+        <Box className="mainContainer" >
             <Flex>
                 <Box className="addressContainer"
-                  w="50%"
+                  w="60%"
                   bgGradient="linear(to-t, white, .500)" 
 
                 >
                     {step === 1 && (
                     <AddressInfo
                         onNext={handleNext}
+                        formData={formData}
+                        setFormData={setFormData}
                     />
                     )}
                     {step === 2 && (
                     <PersonalInfo
                         onNext={handleNext}
                         onPrevious={handlePrevious}
+                        formData={formData}
+                        setFormData={setFormData}
+                        handleSubmit={handleSubmit}
                     />
                     )}
                     {step === 3 && (
+                    <Confirmation
+                        onNext={handleNext}
+                        onPrevious={handlePrevious}
+                        formData={formData}
+                    />
+                    )}
+                    {step ===4  && (
                     <Payment
                         onNext={handleNext}
                         onPrevious={handlePrevious}
@@ -47,8 +67,10 @@ const Address = () => {
                 <Box className="checkoutContainer"
                   bgGradient="linear(to-t, #185E49, teal.500)" 
                     color="white"
-                    w="50%"
+                    w="40%"
+                    
                 >
+                    <CheckOut/>
                     <h1>akash</h1>
                 </Box>
             </Flex>
--- let-it-bee/src/Pages/address/AddressInfo.jsx
+++ let-it-bee/src/Pages/address/AddressInfo.jsx
@@ -1,38 +1,53 @@
-import { Input,Stack,Button,Box, Heading,ButtonGroup,HStack, Flex} from "@chakra-ui/react";
+import { Input,Stack,Button,Box, Heading,ButtonGroup,Flex} from "@chakra-ui/react";
+import { BsArrowLeft } from "react-icons/bs";
 
-const AddressInfo = () =>{
+
+
+
+const AddressInfo = ({onNext , formData ,setFormData}) =>{
+
+    const handleNext = () =>{
+        onNext();
+        console.log("hii")
+    }
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+      };
+    
+
     return(
         <Box  >
            <Stack spacing='45px'> 
             <Flex>
-                <Box h={"50px"} cursor={'pointer'} marginLeft="22%" ><Heading size="lg">LET IT BEE.CO</Heading></Box>
+                <Box h={"50px"} cursor={'pointer'} marginLeft="22%" ><Heading size="lg"><span style={{color:" #185E49"}}>LET IT BEE</span>.CO</Heading></Box>
             </Flex>
            
             <Stack spacing='15px'>
             <Flex>
                 <Box   marginLeft="22%">
-                    <Heading size='s'>ADDRESS/CITY-NAME/POSTAL-CODE/DISTRICT/STATE</Heading>
+                    <Heading size='s'> <span style={{color:" #185E49"}}>DILIVERY </span>/ <span>DETAILS  </span>/<span> CONFIRMATION & PAYMENT </span></Heading>
                 </Box>
             </Flex>
 
             <Box >
                 <Box   w='60%' margin='auto'>
                 <Stack spacing='120px'>
+                    
                     <Box>
                         <Stack spacing={3} p={4}>
-                            <Input variant='flushed' placeholder="ADDRESS" size='md' />
-                            <Input variant='flushed' placeholder="CITY NAME" size='md' />
-                            <Input variant='flushed' placeholder="POSTAL CODE" size='md' />
-                            <Input variant='flushed' placeholder="DISTRICT" size='md' />
-                            <Input variant='flushed' placeholder="STATE" size='md' />
+                            <Input variant='flushed' placeholder="ADDRESS" size='md' marginTop="8px"  name="address" onChange={handleChange} value={formData.address || ""} />
+                            <Input variant='flushed' placeholder="CITY NAME" size='md'  marginTop="8px" name="city"onChange={handleChange} value={formData.city || ""}/>
+                            <Input variant='flushed' placeholder="POSTAL CODE" size='md'marginTop="8px" name="postal" onChange={handleChange} value={formData.postal || ""} />
+                            <Input variant='flushed' placeholder="DISTRICT" size='md'marginTop="8px" name="district" onChange={handleChange} value={formData.district || ""} />
+                            <Input variant='flushed' placeholder="STATE" size='md'marginTop="8px" name="state" onChange={handleChange} value={formData.state || ""}  />
                             
                         </Stack>
                     </Box>
                     <Box  spacing={10} marginBottom="50px">
-                    <ButtonGroup gap='10'>
-                        <Button colorScheme='teal' >BACK</Button>
-                        <Button colorScheme='teal' pl="14" pr="14">PROCEED</Button>
-                    </ButtonGroup>
+                        <ButtonGroup gap='10'>
+                            <Button colorScheme='teal' pl="7" pr="7"   borderRadius='20px' leftIcon={< BsArrowLeft/>}  variant='outline' >BACK</Button>
+                            <Button colorScheme='teal' pl="20" pr="20"  borderRadius='20px'  _hover={{ bg: '#F05A1F' }} onClick={handleNext}>PROCEED</Button>
+                        </ButtonGroup>
                     </Box>
                  </Stack>
                 </Box>
