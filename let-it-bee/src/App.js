193f8ad48f3b9385cd80fe2d3465bc783b7b2180
akashtijare <akashtijare708374@gmail.com>
2023-08-16 15:17:57 +0530

    done

--- let-it-bee/src/App.js
+++ let-it-bee/src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import { ProductDetails } from './Pages/ProductDetails';
-import Address from './Pages/address/Address';
-import ThankYou from './Pages/address/ThankYou';
+
 import { Allroutes } from './Routes/Allroutes';
 
 function App() {
   return (
     <div className="App">
-    {/* <Navbar/>
-    <Allroutes/>  */}
-    <Address/>
+    <Navbar/>
+    <Allroutes/> 
     </div>
   );
 }
